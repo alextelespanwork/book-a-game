0177d22fdda87a78352416ea6abfbfcfd272654a
Alex Telespan <alextelespan@gmail.com>
2017-06-16 17:17:47 +0100

    changed the add football pitch

--- src/app/main/add-football-pitch/add-football-pitch.component.ts
+++ src/app/main/add-football-pitch/add-football-pitch.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'add-football-pitch',
   templateUrl: './add-football-pitch.component.html',
   styleUrls: ['./add-football-pitch.component.scss'],
 })
-export class AddFootballPitchComponent {
+export class AddFootballPitchComponent implements OnInit {
+  pitchTypes: String[];
+  inputName: String = new String();
+  inputAddress: String = new String();
+  inputTypePitch: String = new String();
+   ngOnInit(): void {
+   this.pitchTypes = ['Five a Side',
+                      'Six a Side',
+                      'Seven a Side',
+                      'Eight a Side',
+                      'Nine a Side',
+                      'Ten a Side',
+                      'Eleven a Side',
+                      'Twelve a Side',
+                    ];
+  }
 
+  addPitch() {    
+    console.log(this.inputName);
+    console.log(this.inputAddress);
+    console.log(this.inputTypePitch);
+    
+  }
 
 }
