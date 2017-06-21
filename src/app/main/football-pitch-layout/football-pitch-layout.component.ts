d8690711bf4ab94b4ec487a4810c21d3708f2b4d
Alex Telespan <alextelespan@gmail.com>
2017-06-21 16:17:17 +0100

    added events for pitch buttons

--- src/app/main/football-pitch-layout/football-pitch-layout.component.ts
+++ src/app/main/football-pitch-layout/football-pitch-layout.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class FootballPitchLayoutComponent {
 
+  onClick(event) {
+    console.log(event);
+      console.log(event.srcElement.className);
+      console.log(event.srcElement.firstChild.className);
+
+
+    if (event.srcElement.className.includes('btn-default')) {
+      event.srcElement.className = 'btn btn-warning';
+      event.srcElement.firstChild.className = 'ion-close-round';          
+    } else if (event.srcElement.className.includes('btn-warning')) {
+      event.srcElement.className = 'btn btn-default';
+      event.srcElement.firstChild.className = 'ion-plus-round';      
+    }
+    //console.log(event.srcElement.attributes.id);
+    //var idAttr = event.srcElement.attributes.id;
+    //var value = idAttr.nodeValue;
+    //console.log(value);
+  }
 }
